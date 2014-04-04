cbcf76942b8011578bdda6e22316f1068729d545
Jeffrey Fulton <jeffrey.fulton@me.com>
2014-04-03 20:51:41 -0500

    Remove unnecessary ids from front-page headers

--- wp-content/themes/fashionation/front-page.php
+++ wp-content/themes/fashionation/front-page.php
@@ -6,21 +6,21 @@
 
 	<div class="row">
 		<div class='col-sm-7 col-sm-offset-5'>
-			<h1 id="one" class="section_heading"><?php the_field('section_1_heading'); ?></h1>
+			<h1 class="section_heading"><?php the_field('section_1_heading'); ?></h1>
 			<p class="section_content"><?php the_field('section_1_content'); ?></p>
 		</div>
 	</div>
 
 	<div class="row">
 		<div class='col-sm-7 col-sm-offset-5'>
-			<h1 id="two" class="section_heading"><?php the_field('section_2_heading') ?></h1>
+			<h1 class="section_heading"><?php the_field('section_2_heading') ?></h1>
 			<p class="section_content"><?php the_field('section_2_content'); ?></p>
 		</div>
 	</div>
 
 	<div class="row">
 		<div class='col-sm-7 col-sm-offset-5'>
-			<h1 id="three" class="section_heading"><?php the_field('section_3_heading') ?></h1>
+			<h1 class="section_heading"><?php the_field('section_3_heading') ?></h1>
 			<p class="section_content"><?php the_field('section_3_content'); ?></p>
 		</div>
 	</div>
