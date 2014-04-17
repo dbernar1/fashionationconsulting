50262e4e3f1f483d2c4205611d2032169a7e3ca7
Jeffrey Fulton <jeffrey.fulton@me.com>
2014-04-17 10:24:17 -0500

    Add white space around footer, remove click-to-scroll javascript

--- wp-content/themes/fashionation/js/fashionation.js
+++ wp-content/themes/fashionation/js/fashionation.js
@@ -76,19 +76,4 @@ jQuery(document).ready(function($){
 		$('body').css("visibility", "visible").hide().fadeIn('slow');
 	}, 300);
 
-
-  $('a[href*=#]:not([href=#])').click(function() {
-  	console.log('click');
-    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
-      var target = $(this.hash);
-      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-      if (target.length) {
-        $('html,body').animate({
-          scrollTop: target.offset().top - 20
-        }, 1000);
-        return false;
-      }
-    }
-  });
-
 });
