343b4e98dd97b31047a2245efb5a5a5cd0c27a94
Jeffrey Fulton <jeffrey.fulton@me.com>
2014-04-18 11:47:33 -0500

    Move Services to new Services page by creating custom post type 'service', change front-page text-areas to wysiwyg, modify html and css acordingly

--- wp-content/themes/fashionation/front-page.php
+++ wp-content/themes/fashionation/front-page.php
@@ -7,21 +7,21 @@
 	<div class="row">
 		<div class='col-sm-7 col-sm-offset-5'>
 			<h1 class="section_heading"><?php the_field('section_1_heading'); ?></h1>
-			<p class="section_content"><?php the_field('section_1_content'); ?></p>
+			<div class="section_content"><?php the_field('section_1_content'); ?></div>
 		</div>
 	</div>
 
 	<div class="row">
 		<div class='col-sm-7 col-sm-offset-5'>
 			<h1 class="section_heading"><?php the_field('section_2_heading') ?></h1>
-			<p class="section_content"><?php the_field('section_2_content'); ?></p>
+			<div class="section_content"><?php the_field('section_2_content'); ?></div>
 		</div>
 	</div>
 
 	<div class="row">
 		<div class='col-sm-7 col-sm-offset-5'>
 			<h1 class="section_heading"><?php the_field('section_3_heading') ?></h1>
-			<p class="section_content"><?php the_field('section_3_content'); ?></p>
+			<div class="section_content"><?php the_field('section_3_content'); ?></div>
 		</div>
 	</div>
 
