0562c934748e81c2183e1479850a61aee4c337cc
Jeffrey Fulton <jeffrey.fulton@me.com>
2014-04-22 18:00:24 -0500

    Removed unused images

--- wp-content/themes/fashionation/index.php
+++ wp-content/themes/fashionation/index.php
@@ -6,7 +6,6 @@
 
 	<div class="row">
 		<div class='col-sm-7 col-sm-offset-5'>
-			<p>index.php</p>
 			<h1 class="section_heading"><?php the_title(); ?></h1>
 			<p class="section_content"><?php the_content(); ?></p>
 		</div>
