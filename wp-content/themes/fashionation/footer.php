d09234266ecb17a11876915632ed807fede6c9f5
Jeffrey Fulton <jeffrey.fulton@me.com>
2014-04-25 13:09:56 -0500

    Woman silhouette image not visible on xs device (i.e. phones)

--- wp-content/themes/fashionation/footer.php
+++ wp-content/themes/fashionation/footer.php
@@ -4,7 +4,7 @@
 	<div class="container">
 		<div class="row">
 			<div class="col-sm-5">
-					<img id="woman_silhouette" src="<?php bloginfo('template_directory') ?>/images/woman_silhouette.png" />
+					<img class="hidden-xs" id="woman_silhouette" src="<?php bloginfo('template_directory') ?>/images/woman_silhouette.png" />
 
 					<?php wp_nav_menu( array(
 					      		'theme_location'	=> 'pages-menu',
