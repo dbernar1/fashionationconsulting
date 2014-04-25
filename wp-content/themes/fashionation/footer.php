068a91c138093efaa892be71a688e86c33388a06
Jeffrey Fulton <jeffrey.fulton@me.com>
2014-04-25 13:17:54 -0500

    Remove navigation links from footer

--- wp-content/themes/fashionation/footer.php
+++ wp-content/themes/fashionation/footer.php
@@ -5,22 +5,6 @@
 		<div class="row">
 			<div class="col-sm-5">
 					<img class="hidden-xs" id="woman_silhouette" src="<?php bloginfo('template_directory') ?>/images/woman_silhouette.png" />
-
-					<?php wp_nav_menu( array(
-					      		'theme_location'	=> 'pages-menu',
-									  'depth'       		=> 0,
-										'sort_column' 		=> 'menu_order',
-										'container'				=> 'div',
-										'container_class' => 'footer_nav_container',
-										'container_id'		=> '',
-										'menu_class'  		=> 'footer_nav_ul',
-										'include'     		=> '',
-										'exclude'     		=> '',
-										'echo'        		=> true,
-										'show_home'   		=> false,
-										'link_before' 		=> '',
-										'link_after'  		=> '' ) );
-					?> 
 			</div>
 			<div class="col-sm-7">
 				<h1 class='site-title'><a href='<?php echo esc_url( home_url( '/' ) ); ?>' title='<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>' rel='home'><?php bloginfo( 'name' ); ?></a></h1>
